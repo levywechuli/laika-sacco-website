dee49f289ea42c8b74376197eca69adbdc644ecc
levywechuli <levy.wechuli@student.moringaschool.com>
2025-07-19 23:16:27 +0300

    adding facebook link

--- laika-sacco/src/components/layout/Footer.tsx
+++ laika-sacco/src/components/layout/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
               through innovative savings, loans, and investment solutions.
             </p>
             <div className="flex space-x-4">
-              <a href="#" className="w-10 h-10 bg-primary-light rounded-lg flex items-center justify-center hover:bg-gold transition-colors">
+              <a href="https://www.facebook.com/share/1arnrrAkYM/" className="w-10 h-10 bg-primary-light rounded-lg flex items-center justify-center hover:bg-gold transition-colors">
                 <Facebook className="w-5 h-5" />
               </a>
               <a href="#" className="w-10 h-10 bg-primary-light rounded-lg flex items-center justify-center hover:bg-gold transition-colors">
